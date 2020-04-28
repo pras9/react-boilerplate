71f7ce127fb4cd2e76640d030b110196c885b2cc
Prasun Mukherjee <prasun.mukherjee@publicissapient.com>
2020-04-29 00:20:55 +0530

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div class="main">
+        <h1>Here is the App component!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
